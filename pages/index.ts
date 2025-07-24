2f6d2b163a30bc3b4d6313fa5267b7c14d4972eb
skhmialeuski <skhmialeuski@apple.com>
2025-07-25 00:27:57 +0200

    Update structure and separate Header entity for more scaleability

--- pages/index.ts
+++ pages/index.ts
@@ -1,4 +1,5 @@
 export { BasePage } from './BasePage.page';
+export { HomePage } from './HomePage.page';
 export { HeaderComponent } from './common-components/HeaderComponent.page';
 export { SearchResultsPage } from './SearchResults.page';
 export { MovieCardPage } from './MovieCard.page';
